39985accdb7807ac666cf46fbbb4d98b58288296
Alex Shirin <alexei.shirin@gmail.com>
2017-07-06 14:04:33 +0300

    New records format

--- index.js
+++ index.js
@@ -14,10 +14,18 @@ xlsxj({
     } else {
         let remappedData = _.map(result, function (record) {
             let dtdRules = _.filter(rules, {system:'DTD'});
-            let remappedRecord = {};
+            let remappedRecord = {
+                system:'DTD'
+            };
+            let originalValues = {};
+            let derivedValues = {};
+            
             _.forEach(dtdRules, function(rule){
-                remappedRecord[rule.resultFieldName] = record[rule.originalFieldName];
+                originalValues[rule.resultFieldName] = record[rule.originalFieldName] || "";
+                derivedValues[rule.resultFieldName] = record[rule.originalFieldName] || "";
             });
+            remappedRecord.originalValues = originalValues;
+            remappedRecord.derivedValues = derivedValues;
 
             return remappedRecord;
         });
@@ -37,47 +45,21 @@ xlsxj({
         console.error(err);
     } else {
         let remappedData = _.map(result, function (record) {
+            let dtccRules = _.filter(rules, {system:'DTCC'});
             let remappedRecord = {
-                externalId:record['Trade Reference Identifier'],
-                tradeDate:record['Trade Date'],
-                maturityDate:record['Scheduled Termination Date/Expiration Date'],
-                effectiveDate: record['Effective Date'],
-                counterpartyAccountNumber: record['Counterparty Account Number'],
-                dtccAccountNumber: record['DTCC Account Number'],
-                fixedRate: record['Fixed Rate'],
-                notionalCCY: record['Warehouse Current State Notional Currency'],
-                notionalAMT: record['Warehouse Current State Notional Amount'],
-                independentPayer: record['Independent COUNTERPARTY'],
-                independentReceiver: record['Independent TRADINGPARTY'],
-                fixedRatePayer: record['Fixed Rate Payer (Buyer)/Swaption Buyer'],
-                floatRatePayer: record['Float Rate Payer (Seller)/Swaption Seller'],
-                calculationAgentBusinessCenterCode: record['Calculation Agent Business Center Code'],
-                referenceEntityId: record['Reference Entity ID'],
-                referenceEntityName: record['Reference Entity Name'],
-                independentPercent: record['Independent Percent'],
-                floatRateCurrency: record['Float Rate Currency'],
-                initialPaymentPayer: record['Initial Payment Payer'],
-                initialPaymentReceiver: record['Initial Payment Receiver'],
-                firstPaymentDate: record['First Payment Date'],
-                masterDocumentTransactionType: record['Master Document Transaction Type'],
-                referenceObligation: record['Reference Obligation'],
-                restructuringEvent: record['Restructuring Event'],
-                paymentAmount: record['Single Payment Amount/Premium Payment Amount'],
-                singlePaymentCurrency: record['Single Payment Currency/Premium Payment Currency'],
-                singlePaymentDate: record['Single Payment Date/Premium Payment Date'],
-                bookId: record['Desk ID'],
-                product: record['Product Type'],
-                annexDate: record['Master Confirm Annex Date'],
-                optionMaturity: record['Underlying Scheduled Termination Date'],
-                strike: record['Strike Price'],
-                exhaustionPoint: record['Exhaustion Point'],
-                attachmentPoint: record['Attachment Point'],
-                referencePolicyApplicable: record['Reference Policy Applicable'],
-                entityType: record['Product ID'],
-                subCategory: record['Sub-Product']
+                system:'DTCC'
             };
+            let originalValues = {};
+            let derivedValues = {};
 
-            return record;
+            _.forEach(dtccRules, function(rule){
+                originalValues[rule.resultFieldName] = record[rule.originalFieldName] || "";
+                derivedValues[rule.resultFieldName] = record[rule.originalFieldName] || "";
+            });
+            remappedRecord.originalValues = originalValues;
+            remappedRecord.derivedValues = derivedValues;
+
+            return remappedRecord;
         });
         fs.writeFile('DTCC.json', JSON.stringify(remappedData), function (err) {
             if (err) throw err;
